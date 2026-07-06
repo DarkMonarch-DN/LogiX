65de5b52e57b75203894b7f9e7b9ee34bd384b19
Sovana <d9768368@gmail.com>
2026-07-06 17:24:36 +0300

    setting database

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,13 +4,19 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 import { PrismaClient } from 'generated/prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor(private readonly configService: ConfigService) {
-        const adapter = new PrismaPg({
+        const pool = new Pool({
             connectionString: configService.get<string>('DATABASE_URL'),
+            max: 10,
+            idleTimeoutMillis: 10000,
         });
+
+        // 2. Передаем пул в адаптер Prisma v7
+        const adapter = new PrismaPg(pool);
 
         super({ adapter });
     }
